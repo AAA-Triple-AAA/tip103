f5f915266ed0901d02e3bb1681dcb09bfa215472
Adrian Abella <123528561+AAA-Triple-AAA@users.noreply.github.com>
2026-02-24 19:31:18 -0500

    Added W1S1 problems

---
+++ week1/session1/problem_set1/problem5.py
@@ -0,0 +1,48 @@
+"""
+Problem 5: Missing Clues
+
+Christopher Robin set up a scavenger hunt for Pooh, but it's a blustery 
+day and several hidden clues have blown away. Write a function 
+find_missing_clues() to help Christopher Robin figure out which clues he 
+needs to remake. The function accepts two integers lower and upper and a 
+unique integer array clues. All elements in clues are within the inclusive 
+range [lower, upper].
+
+A clue x is considered missing if x is in the range [lower, upper] and x 
+is not in clues.
+
+Return the shortest sorted list of ranges that exactly covers all the 
+missing numbers. That is, no element of clues is included in any of the 
+ranges, and each missing number is covered by one of the ranges.
+
+Example Usage:
+clues = [0, 1, 3, 50, 75]
+lower = 0
+upper = 99
+find_missing_clues(clues, lower, upper)
+
+clues = [-1]
+lower = -1
+upper = -1
+find_missing_clues(clues, lower, upper)
+
+Example Output:
+[[2, 2], [4, 49], [51, 74], [76, 99]]
+[]
+"""
+
+
+def find_missing_clues(clues: list[int], lower: int, upper: int) -> list[list[int]]:
+    return []
+
+
+if __name__ == "__main__":
+    clues = [0, 1, 3, 50, 75]
+    lower = 0
+    upper = 99
+    print(find_missing_clues(clues, lower, upper))
+
+    clues = [-1]
+    lower = -1
+    upper = -1
+    print(find_missing_clues(clues, lower, upper))
---
+++ week1/session1/problem_set2/problem1.py
@@ -0,0 +1,42 @@
+"""
+Problem 1: Words Containing Character
+Write a function words_with_char() that accepts a list of strings words and a 
+character x. Return a list of indices representing the words that contain the 
+character x. The returned list may be in any order.
+
+Example Usage:
+words = ["batman", "superman"]
+x = "a"
+words_with_char(words, x)
+
+words = ["black panther", "hulk", "black widow", "thor"]
+x = "a"
+words_with_char(words, x)
+
+words = ["star-lord", "gamora", "groot", "rocket"]
+x = "z"
+words_with_char(words, x)
+
+Example Output:
+[0, 1]
+[0, 2]
+[]
+"""
+
+
+def words_with_char(words: list[str], x: str) -> list[int]:
+    return []
+
+
+if __name__ == "__main__":
+    words = ["batman", "superman"]
+    x = "a"
+    print(words_with_char(words, x))
+
+    words = ["black panther", "hulk", "black widow", "thor"]
+    x = "a"
+    print(words_with_char(words, x))
+
+    words = ["star-lord", "gamora", "groot", "rocket"]
+    x = "z"
+    print(words_with_char(words, x))
